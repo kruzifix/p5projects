78287581fb85ca2d949533b257cdb24b8a9e82ba
David Cukrowicz <david.cukro@gmail.com>
2019-12-24 10:28:26 +0100

    piece movemetn and rotation

--- sketches/hextris/hexgrid.js
+++ sketches/hextris/hexgrid.js
@@ -1,117 +1,131 @@
 class HexGrid
 {
-	constructor(w, h) {
-		this.w = w;
-		this.h = h;
+    // drawOpts: size, xOff, yOff
+    constructor(w, h, drawOpts) {
+        this.w = w;
+        this.h = h;
 
-		const bottomCenter = axial2cube(offset2axial({ c: Math.floor(w / 2), r: h - 2 }));
+        drawOpts.sc = Math.floor(drawOpts.size * 1.5);
+        drawOpts.sr = Math.floor(drawOpts.size * sqrt(3));
+        drawOpts.hw = Math.floor(drawOpts.size * 0.5);
+        drawOpts.hh = Math.floor(drawOpts.sr * 0.5);
 
-		this.cells = new Array(h);
-		for (let r = 0; r < h; r++) {
-			this.cells[r] = new Array(w);
-			for (let c = 0; c < w; c++) {
-				let active = true;
-				if (c == 0 || c == w - 1) {
-					active = false;
-				} else if (r == 0 || r == h - 1) {
-					active = false;
-				}
-				const off = { c, r };
-				const cube = axial2cube(offset2axial(off));
+        this.drawOpts = drawOpts;
 
-				if (cube.y < bottomCenter.y || cube.z > bottomCenter.z) {
-					active = false;
-				}
+        const bottomCenter = axial2cube(offset2axial({ c: Math.floor(w / 2), r: h - 2 }));
 
-				this.cells[r][c] = {
-					active,
-					filled: false,
-				};
-			}
-		}
-	}
+        this.cells = new Array(h);
+        for (let r = 0; r < h; r++) {
+            this.cells[r] = new Array(w);
+            for (let c = 0; c < w; c++) {
+                let active = true;
+                if (c == 0 || c == w - 1) {
+                    active = false;
+                } else if (r == 0 || r == h - 1) {
+                    active = false;
+                }
+                const off = { c, r };
+                const cube = axial2cube(offset2axial(off));
 
-	// offset
-	get(c, r) {
-		return this.cells[r][c];
-	}
+                if (cube.y < bottomCenter.y || cube.z > bottomCenter.z) {
+                    active = false;
+                }
 
-	draw(size, xOff, yOff) {
-		const sc = Math.floor(size * 1.5);
-		const sr = Math.floor(size * sqrt(3));
-		const hw = Math.floor(size * 0.5);
-		const hh = Math.floor(sr * 0.5);
+                this.cells[r][c] = {
+                    active,
+                    filled: false,
+                    fillColor: [0, 0, 0]
+                };
+            }
+        }
+    }
 
-		stroke(0);
-		fill(200, 0, 0);
-		textAlign(CENTER, CENTER);
-		for (let c = 0; c < this.w; c++) {
-			for (let r = 0; r < this.h; r++) {
-				const cell = this.get(c, r);
+    // offset
+    get(c, r) {
+        return this.cells[r][c];
+    }
 
-				//if (!cell.active)
-				//	continue;
+    getCube(cube) {
+        const off = cube2offset(cube);
+        return this.get(off.c, off.r);
+    }
 
-				const px = Math.floor(xOff + sc * c);
-				const py = Math.floor(yOff + sr * (r + 0.5 * (c & 1)));
+    drawHex(c, r) {
+        const px = Math.floor(this.drawOpts.xOff + this.drawOpts.sc * c);
+        const py = Math.floor(this.drawOpts.yOff + this.drawOpts.sr * (r + 0.5 * (c & 1)));
+        fillHex(px, py, this.drawOpts.hw, this.drawOpts.hh);
+    }
 
-				if (cell.active)
-				{
-					if (cell.filled)
-						fill(0, 200, 0);
-					else
-						fill(222);
-				}
-				else
-					fill(50);
+    draw() {
+        stroke(0);
+        fill(200, 0, 0);
+        textAlign(CENTER, CENTER);
+        for (let c = 0; c < this.w; c++) {
+            for (let r = 0; r < this.h; r++) {
+                const cell = this.get(c, r);
 
-				//circle(px, py, size * 0.85);
-				fillHex(px, py, hw, hh);
+                if (!cell.active)
+                    continue;
 
-				// const coord = { c, r };
-				// const axial = offset2axial(coord);
-				// const cube = axial2cube(axial);
-				// fill(200, 0, 0);
-				// text(c + "|" + r, px, py - size * 0.5);
-				// text(axial.q + "|" + axial.r, px, py);
-				// text(cube.x + "|" + cube.y + "|" + cube.z, px, py + size * 0.5);
-			}
-		}
-	}
+                if (cell.filled)
+                    fill(...cell.fillColor);
+                else
+                    fill(r % 2 == 0 ? 166 : 222);
+
+                this.drawHex(c, r);
+            }
+        }
+    }
 }
 
 function fillHex(cx, cy, rw, rh) {
-	push();
-	translate(cx, cy);
-	beginShape();
-	vertex(-rw, -rh);
-	vertex(rw, -rh);
-	vertex(2 * rw, 0);
-	vertex(rw, rh);
-	vertex(-rw, rh);
-	vertex(-2 * rw, 0);
-	endShape(CLOSE);
-	pop();
+    push();
+    translate(cx, cy);
+    beginShape();
+    vertex(-rw, -rh);
+    vertex(rw, -rh);
+    vertex(2 * rw, 0);
+    vertex(rw, rh);
+    vertex(-rw, rh);
+    vertex(-2 * rw, 0);
+    endShape(CLOSE);
+    pop();
 }
 
 function axial2offset(axial) {
-	return {
-		c: axial.q,
-		r: axial.r + (axial.q - (axial.q & 1)) / 2,
-	};
+    return {
+        c: axial.q,
+        r: axial.r + (axial.q - (axial.q & 1)) / 2,
+    };
 }
 
 function axial2cube(axial) {
-	return {
-		x: axial.q,
-		y: -axial.q - axial.r,
-		z: axial.r,
-	};
+    return {
+        x: axial.q,
+        y: -axial.q - axial.r,
+        z: axial.r,
+    };
 }
 
 function offset2axial(offset) {
-	return {
-		q: offset.c,
-		r: offset.r - (offset.c - (offset.c & 1)) / 2,
-	};
+    return {
+        q: offset.c,
+        r: offset.r - (offset.c - (offset.c & 1)) / 2,
+    };
+}
+
+function offset2cube(offset) {
+    return axial2cube(offset2axial(offset));
+}
+
+function cube2offset(cube) {
+    return axial2offset({ q: cube.x, r: cube.z });
+}
+
+function addCube(a, b) {
+    return {
+        x: a.x + b.x,
+        y: a.y + b.y,
+        z: a.z + b.z,
+    };
 }
--- sketches/hextris/sketch.js
+++ sketches/hextris/sketch.js
@@ -1,20 +1,171 @@
 let grid;
 
+let originalPieces;
+let pieceOrigin;
+
+let piece;
+let piecePos;
+let pieceColor;
+
+let reqPiece;
+let reqPiecePos;
+
+let heartBeatTimer = 0;
+let heartBeatPeriod = 1.5;
+
 function setup() {
-	createCanvas(800, 700);
+    createCanvas(800, 700);
 
-	grid = new HexGrid(11, 11);
+    const size = 40;
+    const w = 11;
+    const xOff = width / 2 - size * 1.5 * (w - 1) / 2;
+    grid = new HexGrid(w, 11, {
+        size,
+        xOff,
+        yOff: -20,
+    });
 
-	grid.get(1, 4).filled = true;
-	grid.get(2, 3).filled = true;
-	grid.get(6, 7).filled = true;
-	grid.get(4, 2).filled = true;
+    originalPieces = [
+        {
+            piece: [
+                { x: 0, y: 1, z: -1 },
+                { x: 0, y: 0, z: 0 },
+                { x: 0, y: -1, z: 1 },
+            ],
+            color: [30, 168, 199],
+        },
+        {
+            piece: [
+                { x: 1, y: 0, z: -1 },
+                { x: 0, y: 0, z: 0 },
+                { x: 0, y: -1, z: 1 },
+            ],
+            color: [199, 145, 30],
+        },
+    ];
+    pieceOrigin = offset2cube({ c: 5, r: 3 });
+    newPiece();
 }
 
 function draw() {
-	background(0);
+    // const dt = millis() * 0.001;
+    // heartBeatTimer += dt;
+    // if (heartBeatTimer >= heartBeatPeriod) {
+    //     heartBeatTimer -= heartBeatPeriod;
 
-	const size = 40;
-	const xoff = width / 2 - size * 1.5 * (grid.w - 1) / 2;
-	grid.draw(size, xoff, -20);
+    //     moveDown();
+    // }
+
+    background(0);
+
+    grid.draw();
+
+    for (const p of piece) {
+        const pos = cube2offset(addCube(piecePos, p));
+        stroke(0);
+        fill(...pieceColor);
+        grid.drawHex(pos.c, pos.r);
+    }
+}
+
+function keyPressed() {
+    if (key == 'j') {
+        requestRotate(false);
+    }
+    if (key == 'k') {
+        requestRotate(true);
+    }
+    if (key == 's') {
+        moveDown();
+    }
+    if (key == 'd') {
+        let offsetPos = cube2offset(piecePos);
+        offsetPos.c += 1;
+        requestMove(offset2cube(offsetPos));
+    }
+    if (key == 'a') {
+        let offsetPos = cube2offset(piecePos);
+        offsetPos.c -= 1;
+        requestMove(offset2cube(offsetPos));
+    }
+}
+
+function newPiece() {
+    piecePos = Object.assign({}, pieceOrigin);
+    const index = Math.floor(random() * originalPieces.length);
+    piece = originalPieces[index].piece;
+    pieceColor = originalPieces[index].color;
+
+    // TODO: check for game over
+}
+
+function moveDown() {
+    let reqPos = Object.assign({}, piecePos);
+    reqPos.y -= 1;
+    reqPos.z += 1;
+
+    if (!requestMove(reqPos)) {
+        // touch down!
+        // TODO: fill grid
+        for (let p of piece) {
+            const cell = grid.getCube(addCube(piecePos, p));
+            cell.filled = true;
+            cell.fillColor = pieceColor;
+        }
+
+        // TODO: check clears, points!
+        newPiece();
+
+        heartBeatTimer = 0;
+    }
+}
+
+// toPos: cube
+function requestMove(toPos) {
+    for (const p of piece) {
+        const cell = grid.getCube(addCube(toPos, p));
+        if (!cell.active || cell.filled) {
+            return false;
+        }
+    }
+    piecePos = Object.assign({}, toPos);
+    return true;
+}
+
+function requestRotate(right) {
+    const newPiece = right ? rotateRight(piece) : rotateLeft(piece);
+    for (const p of newPiece) {
+        const cell = grid.getCube(addCube(piecePos, p));
+        if (!cell.active || cell.filled) {
+            return false;
+        }
+    }
+    piece = newPiece;
+    return true;
+}
+
+function rotateRight(piece) {
+    return rotatePiece(piece, (x, y, z) => ({
+        x: -z,
+        y: -x,
+        z: -y,
+    }));
+}
+
+function rotateLeft(piece) {
+    return rotatePiece(piece, (x, y, z) => ({
+        x: -y,
+        y: -z,
+        z: -x,
+    }));
+}
+
+function rotatePiece(piece, rotFunc) {
+    let res = [];
+
+    for (const p of piece) {
+        res.push(rotFunc(p.x, p.y, p.z));
+    }
+
+    return res;
 }
